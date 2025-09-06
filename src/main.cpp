e4212e33ad75af0ade80354b6c346995aa045312
David-Naray <modellingartsyt@gmail.com>
2025-09-06 18:53:26 +0100

    can run the program with a couple parameters, one to record for samples, one for training and default anything else for listening

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 #include "portaudio.h"
 
-int main() {
-    std::cout << "Hello, audio world! poopy!" << std::endl;
+int main(int argc, char* argv[]) {//argc is number of params, char* argv is an array of pointers to characters ie, a word
+    if (argc == 3 && std::string(argv[1]) == "--record") {
+        std::string label = argv[2];
+        // recordAudio(label);
+        std::cout << "recording!!!" << std::endl;
+    } else if (argc == 2 && std::string(argv[1]) == "--train") {
+        // trainModel();
+        std::cout << "training!!!!" << std::endl;
+    } else {
+        // listenAndRecognize();
+        std::cout << "listening!!!!!!" << std::endl;
+    }
+    // std::cout << "Hello, audio world! poopy!aaaaa" << std::endl;
     return 0;
 }
